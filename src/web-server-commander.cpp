b8f6b83c199975ff06428e0e541ff81790812196
errorcode4319 <sandlekang@gmail.com>
2021-11-27 20:48:21 +0900

    add help command

--- src/web-server-commander.cpp
+++ src/web-server-commander.cpp
@@ -2,11 +2,23 @@
 
 
 void WebServerCommander::run(std::string_view cmdline) {
+    if(cmdline == "help") {
+        help();
+        return;
+    }
     if(auto iter=mCommandMap.find(cmdline.data()); iter != std::end(mCommandMap)) {
-        auto func = iter->second;
+        auto&[func, _] = iter->second;
         func();
     } else {
         std::cerr << "Invalid Command => \'" << cmdline << '\'' << std::endl;
+    }
+}
+
+void WebServerCommander::help() {
+    for(auto& iter : mCommandMap) {
+        auto& cmd = iter.first;
+        auto&[_, desc] = iter.second;
+        std::cout << "- " << cmd << " => " << desc << std::endl;
     }
 }
 
@@ -15,17 +27,17 @@ void WebServerCommander::init() {
         std::runtime_error("Was is nullptr");
     }
     
-    addCommand("run",       [&](){was->run();});
-    addCommand("stop",      [&](){was->stop();});
-    addCommand("state",     [&](){std::cout << was->getState() << std::endl;});
-    addCommand("restart",   [&](){was->restart();});
-    addCommand("reload",    [&](){was->reload();});
-    addCommand("quit",      [&](){exit(0);});
+    addCommand("run",       [&](){was->run();}, "Start HTTP Server");
+    addCommand("stop",      [&](){was->stop();},"Stop HTTP Server");
+    addCommand("state",     [&](){std::cout << was->getState() << std::endl;},"Show Service Info");
+    addCommand("restart",   [&](){was->restart();}, "Restart HTTP Server");
+    addCommand("reload",    [&](){was->reload();}, "Auto reload html pages");
+    addCommand("quit",      [&](){exit(0);}, "Quit Application");
 }
 
-void WebServerCommander::addCommand(std::string_view cmd, std::function<void()> func) {
+void WebServerCommander::addCommand(std::string_view cmd, std::function<void()> func, std::string desc) {
     if(auto iter=mCommandMap.find(cmd.data()); iter == std::end(mCommandMap)) {
-        mCommandMap.insert({cmd.data(), func});
+        mCommandMap.insert({cmd.data(), pack(func, desc)});
     } else {
         std::stringstream ss;
         ss << "Add Command Failed: " << cmd << " is already exists" << std::endl;
--- src/web-server-commander.hpp
+++ src/web-server-commander.hpp
@@ -16,12 +16,13 @@ public:
     }
     
     void run(std::string_view cmdline);
-    
+
 private:
+    void help();
     void init();
-    void addCommand(std::string_view cmd, std::function<void()> func);
+    void addCommand(std::string_view cmd, std::function<void()> func, std::string desc);
     std::shared_ptr<WebServer> was;
-    std::unordered_map<std::string, std::function<void()>> mCommandMap;
+    std::unordered_map<std::string, pkg<std::function<void()>, std::string>> mCommandMap;
 };
 
 #endif // _WEB_SERVER_CMD_HPP
